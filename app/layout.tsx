ac6d2aa7f553a257e4e10a5c05d16ccea72c3e24
Aurea <aurealet20146@gmail.com>
2026-05-04 23:21:12 -0300

    feat: create root layout with accessibility components, SEO metadata, and analytics integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import Header from "@/components/layout/Header";
 import SkipLink from "@/components/acessibilidade/SkipLink";
@@ -94,6 +96,9 @@ export default function RootLayout({
 
         {/* Ferramentas de Inclusão */}
         <VLibras />
+
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
